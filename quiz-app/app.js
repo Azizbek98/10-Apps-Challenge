9178ddd2339b1d5eeee0cae90f7f772646b8fc30
Azizbek98 <azizbeksamatov98@gmail.com>
2021-06-21 13:08:15 +0500

    feat: updating functionality

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -41,12 +41,28 @@ const quizData = [
     }
 ];
 
+const questionElement = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
 let currentQuestion = 0;
 
 loadQuiz();
 
 function loadQuiz(){
+    const currentQuizData = quizData[currentQuestion];
+    questionElement.innerHTML = currentQuizData.question;
 
-
-    currentQuestion++;
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
 }
+
+submitBtn.addEventListener('click', () => {
+    currentQuestion++;
+    loadQuiz();
+})
