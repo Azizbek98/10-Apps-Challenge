264a14e46a9d40cfed1d8829e54e19783eefbb80
Azizbek98 <azizbeksamatov98@gmail.com>
2021-06-23 00:01:33 +0500

    feat: updating some changes

--- note-app/app.js
+++ note-app/app.js
@@ -2,6 +2,10 @@ const noteElement = document.querySelector('.notes');
 const editBtn = document.querySelector('.edit');
 const deleteBtn = document.querySelector('.delete');
 
+const main = noteElement.querySelector('.main');
+const textArea = noteElement.querySelector('textarea');
+
 editBtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
+    textArea.classList.toggle('hidden');
 })
