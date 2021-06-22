4807023ad2a6ab09967a98927f132e7f8e2e2707
Azizbek98 <azizbeksamatov98@gmail.com>
2021-06-22 23:58:14 +0500

    feat: updating functionality

--- note-app/app.js
+++ note-app/app.js
@@ -1,2 +1,7 @@
+const noteElement = document.querySelector('.notes');
 const editBtn = document.querySelector('.edit');
 const deleteBtn = document.querySelector('.delete');
+
+editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden');
+})
